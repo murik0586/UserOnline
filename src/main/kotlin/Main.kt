8be85729e04e962ac548da8928d607ed375f6fe2
murik0586 <murikmusaev86@gmail.com>
2024-07-06 13:48:00 +0300

    Step 3 All steps completed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,43 +1,42 @@
 const val HOUR = 60 * 60
 const val MINUTE = 60
-const val DAYS = 24 * HOUR
+const val DAY = 24 * HOUR
 
 var zeroMinuteInterval = 0..60
 fun main() {
-    val timeOffline = 121
+    val timeOffline = 79200
     println("Lion ${agoToText(timeOffline)}")
 
 }
 
 
 fun agoToText(timeOffline: Int): String {
-    //TODO осталась дополнительная функция отвечающая за: ЧАС, ЧАСА, ЧАСОВ,
     return when (timeOffline) {
         in zeroMinuteInterval -> "был(а) в сети только что"
         in (MINUTE + 1)..HOUR -> "был(а) ${formWordMinutesTheText(timeOffline)} назад"
-        in (HOUR + 1)..DAYS -> "был(а) x часов назад"
-        in (DAYS + 1)..(DAYS * 2) -> "был(а) вчера"
-        in (DAYS * 2)..(DAYS * 3) -> "был(а) позавчера"
+        in (HOUR + 1)..DAY -> "был(а) ${formWordHourText(timeOffline)} назад"
+        in (DAY + 1)..(DAY * 2) -> "был(а) вчера"
+        in (DAY * 2)..(DAY * 3) -> "был(а) позавчера"
         else -> "был(а) давно"
+    }
+}
+
+fun formWordHourText(timeOffline: Int): String {
+    val hour = timeOffline / HOUR
+    return when {
+        hour % 100 in 11..20 -> "$hour часов"
+        hour % 10 == 1 -> "$hour час"
+        hour % 10 in 2..4 ->"$hour часа"
+        else -> "$hour часов"
     }
 }
 
 fun formWordMinutesTheText(timeOffline: Int): String {
     val minutes = timeOffline / 60
-    val lastDigit = minutes % 10
-    val lastTwoDigit = minutes % 100
-
     return when {
-        lastTwoDigit in 11..19 -> "$minutes минут"
-        lastDigit == 1 -> "$minutes минуту"
-        lastDigit in 2..4 -> "$minutes минуты"
+        minutes % 100 in 11..19 -> "$minutes минут"
+        minutes % 10 == 1 -> "$minutes минуту"
+        minutes % 10 in 2..4 -> "$minutes минуты"
         else -> "$minutes минут"
     }
 }
-/*
-Для часов:
-
-1, 21 час назад
-2, 3, 4, 22, 23, 24 часа назад
-5-20 часов назад.
-Итог: у вас должен быть репозиторий на GitHub, в котором будет ваш Gradle-проект. */
